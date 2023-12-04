1490a2e4f74c16bc26b89f68cf7c4aae16697e87
jerry2132 <sumitsinghrawat34@gmail.com>
2023-12-04 22:36:38 +0530

    commit applied

--- src/main/java/com/example/controller/SignUpController.java
+++ src/main/java/com/example/controller/SignUpController.java
@@ -1,14 +1,30 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.entity.User;
 
 @Controller
 public class SignUpController {
 
 	@RequestMapping("/signup")
-	public String signUp() {
-		
+	public String signUp(Model model) {
+		model.addAttribute("user", new User());
 		return "signup";
 	}
+	
+	@RequestMapping(value = "/register",method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("user") User user,@RequestParam(value = "acceptTerms" , defaultValue = "false") boolean acceptTerms, Model model) {
+		
+		
+	System.out.println("Agreement " +acceptTerms);
+	System.out.println("User "+user);
+	return "signUp";
+	
+}
 }
--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -109,6 +109,12 @@ public class User {
 	public void setContacts(List<Contact> contacts) {
 		this.contacts = contacts;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role
+				+ ", enabled=" + enabled + ", imgUrl=" + imgUrl + ", about=" + about + ", contacts=" + contacts + "]";
+	}
 	
 	
 }
