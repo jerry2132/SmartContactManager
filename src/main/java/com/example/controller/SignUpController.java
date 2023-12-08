7ebe2a86d278fe597cf1ae54016aede28609c6a4
jerry2132 <sumitsinghrawat34@gmail.com>
2023-12-08 09:24:03 +0530

    commit applied

--- src/main/java/com/example/controller/SignUpController.java
+++ src/main/java/com/example/controller/SignUpController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,8 +11,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.entity.User;
+import com.example.repository.UserRepository;
 import com.example.service.UserService;
 
 @Controller
@@ -18,6 +22,9 @@ public class SignUpController {
 
 	 	@Autowired
 	    private UserService userService;
+	 	
+	 	@Autowired
+	 	private UserRepository userRepository;
 	 
 	 	@RequestMapping("/signup")
 	 	public String signUp(Model model) {
@@ -27,9 +34,22 @@ public class SignUpController {
 	 	}
 	 	
 	 	@PostMapping("/signup")
-	 	public String registerUser(@ModelAttribute("user") User user,Model model) {
+	 	public String registerUser(@ModelAttribute("user") User user,RedirectAttributes redirectAttributes) {
+	 		
+	 		Optional<User> finduser = userRepository.findUserByEmail(user.getEmail());
+	 		
+	 		if(finduser.isPresent()) {
+	 			
+//	 			model.addAttribute("message", "user already present");
+	 			
+	 			redirectAttributes.addFlashAttribute("errorMessage", "User already present");
+	 			
+	 			return"redirect:/signup";
+	 		}
 	 		
 	 		userService.save(user);
+//	 		model.addAttribute("message", "user added successfully");
+	 		redirectAttributes.addFlashAttribute("successMessage", "User saved succcessfully");
 	 		return "redirect:/signup";
 	 	}
 //	 	@RequestMapping("/signup")
--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.example.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User ,Integer>{
 	
+	Optional<User> findUserByEmail(String email);
 }
