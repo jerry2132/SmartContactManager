a5013f6fa6bb7edd7c2cb4b90263a1bc88961b28
jerry2132 <sumitsinghrawat34@gmail.com>
2024-02-05 23:47:50 +0530

    forgot password module

--- src/main/java/com/example/controller/ForgotPasswordController.java
+++ src/main/java/com/example/controller/ForgotPasswordController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.entity.ForgotPassword;
@@ -78,6 +80,10 @@ public class ForgotPasswordController {
 		
 		forgotPasswordRepository.save(forgotPassword);
 		
+		
+		 //String emailLink = "http://localhost:8080/verify-Otp?email=" + email + "&token=" + forgotPassword.getToken();
+		  
+		
 		try {
 	        // Send OTP instead of email link
 	        forgotPasswordService.sendOtp(user.getEmail(), "Password Reset OTP", forgotPassword.getOtp());
@@ -87,8 +93,22 @@ public class ForgotPasswordController {
 	        return "forgotPassword";
 	    }
 		
+//		model.addAttribute("email", email);
+		return "redirect:/Otp-page?success&email=" + email;
+	}
+	
+	
+	
+	@GetMapping("/Otp-page")
+	public String showOtpPage(@RequestParam("email")String email, Model model) {
+		
+//		System.out.println("otp email  " + email);
+		model.addAttribute("email", email);
 		return "otpPage";
 	}
+//	
+//	@RequestMapping(value = "/verify-Otp", method = {RequestMethod.GET, RequestMethod.POST})
+	
 	
 	@PostMapping("/verify-Otp")
 	public String verifyOtp(@RequestParam("otp")String enteredOtp,@RequestParam("email")String email,
@@ -97,21 +117,23 @@ public class ForgotPasswordController {
 			
 			
 //			UserDetails userDetails  = customUserDetailsServiceImpl.loadUserByUsername(principal.getName());
-//			//User user = userService.findByEmail(userDetails.getUsername());
+//			//User user = userService.findByEmail(userDetails.getUsername());*******,
+//		@RequestParam("email")String email
 //			String email = userDetails.getUsername();
 //			System.out.println("verify-otp mail"+email);
 //			model.addAttribute("email", email);
 ////			model.addAttribute("user", user);
 			
 		
+//		 User user = (User) model.getAttribute("user");
+		 
 
-	        if (forgotPasswordService.verifyOtp(email, enteredOtp)) {
-	            return "newPassword";
-	        } else {
-	            model.addAttribute("error", "Invalid Otp");
-	            return "otpPage";
-	        }
-	    
+			 if (forgotPasswordService.verifyOtp(email, enteredOtp)) {
+		            return "newPassword";
+		        }else {
+		            model.addAttribute("error", "Invalid Otp ");
+		            return "otpPage";
+		        }
 
 			
 		}
